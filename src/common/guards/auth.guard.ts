4663568f0d75de8edef330ca6c9afbb6681488ef
Chaoyi Hu <chaoyihu.work@gmail.com>
2023-12-02 19:49:23 -0800

    Fix bug: 401 unauthrized error when creating new user. Solution: Skipped authentication for user creation. Reason: Since there is no credential to verify against before the user is actually created, authentication for user creation should be skipped. Method: - Enabled global authentication. - Created SkipAuth() decorator. - Use Reflector to find "skipAuth" metadata and skip authentication when found. - Wrap user creationmethod with the decorator. Ref: https://docs.nestjs.com/security/authentication#enable-authentication-globally

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -1,9 +1,27 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class GqlAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super(); 
+  }
+
+  canActivate(context: ExecutionContext) {
+    const skipAuth = this.reflector.get<boolean>(
+      'skipAuth',
+      context.getHandler()
+    );
+
+    if (skipAuth) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
